0e4e379a5fa047ee628ee473586b2e5bffae9333
bambi <samug14@gmail.com>
2021-11-11 23:53:35 +0000

    vector-based movement

--- Ant.py
+++ Ant.py
@@ -1,39 +1,49 @@
 import numpy as np
+from pygame.draw import rect
+from pygame import Rect
 
 """
-implement vector-based movement!
+Now. position += speed, speed += accel, accel = sum of all forces. kinda works.
 """
 
 class Ant:
 
-    def __init__(self, x, y, screen, terrain):
-        self.x = x
-        self.y = y
-        self.position = np.array([self.x, self.y])
+    def __init__(
+            self,
+            x,
+            y,
+            screen,
+            terrain,
+            color = (200,0,0)):
+        self.position = np.array([x, y])
         self.speed = np.array([0,0])
         self.screen = screen
         self.terrain = terrain
         self.stats = {
             'health': 100,
             }
-        self.vectors = {
+        self.forces = {
             'food': np.array([0, 0]),
             'home': np.array([0, 0]),
-            'pheromone?': np.array([0,0])
+            'pheromone?': np.array([0, 0])
             }
+        self.test_force = np.array([0, 0])
+        self.color = color
+        self.nearby = []
 
     def move(self, dx=0, dy=0):
         """depraciated"""
-        self.x += dx
-        self.y += dy
+        self.position[0] += dx
+        self.position[1] += dy
 
     def update(self):
-        self.speed = 'sum_of_all_vectors'
+        acceleration = self.test_force #sum of all forces later
+        self.speed = acceleration
+        np.clip(self.speed, -3, 3, self.speed) #limit or it'll shoot into infinity
         self.position += self.speed
 
     def show(self):
-        # self.screen.pokaż_mnie!
-        pass
+        rect(self.screen, self.color, Rect(self.position[0], self.position[1], 1, 1))
 
     def debug(self):
-        print(self.x, self.y)
+        print(self.position, self.speed)
--- main.py
+++ main.py
@@ -6,23 +6,45 @@ screen = pygame.display.set_mode((400, 400))
 clock = pygame.time.Clock()
 DONE = False
 PAUSE = False
+terrain = None
 
-test_ant = Ant(200, 200)
+# test_ants = [Ant(
+#                200,
+#                200,
+#                screen,
+#                terrain,
+#                color = (
+#                    random.randint(0, 255),
+#                    random.randint(0, 255),
+#                    random.randint(0, 255),))
+#            for x in range(200)]
 
-while not DONE:
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                PAUSE = not PAUSE
-            if event.key == pygame.K_ESCAPE:
-                DONE = True
+test_ant = Ant(200, 200, screen, terrain)
 
-    if not PAUSE:
-        screen.fill((0,0,0))
-        test_ant.move(random.randint(-1,1), random.randint(-1,1))
-        test_ant.debug()
 
-        pygame.draw.rect(screen, (200,0,0), pygame.Rect(test_ant.x, test_ant.y, 3, 3))
+if __name__ == "__main__":
+    while not DONE:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    PAUSE = not PAUSE
+                if event.key == pygame.K_ESCAPE:
+                    DONE = True
 
-        pygame.display.flip()
-        clock.tick(30)
+        if not PAUSE:
+
+            mousex, mousey = pygame.mouse.get_pos()
+            test_ant.test_force[0] = mousex - test_ant.position[0]
+            test_ant.test_force[1] = mousey - test_ant.position[1]
+            screen.fill((0,0,0))
+            test_ant.update()
+            test_ant.show()
+            test_ant.debug()
+#            for ant in test_ants:
+#                ant.move(random.randint(-1,1), random.randint(-1,1))
+#                # ant.debug()
+#                ant.show()
+
+            pygame.display.flip()
+            clock.tick(20)
+
